0d09fa9005bbe76585c11121db0752d40c80dba6
endfrey <teraphattong_43@hotmail.com>
2025-03-23 01:16:44 +0700

    ff

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "@/models/User";
+import User from "@/model/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcard-next" });
