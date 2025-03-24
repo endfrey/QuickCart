28ce5d301b25ef71fb67e382c2ab97caee0e33fb
endfrey <teraphattong_43@hotmail.com>
2025-03-24 14:39:43 +0700

    swwww

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,14 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { syncUserCration, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { inngest, syncUserCration, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
+
     syncUserCration,
     syncUserUpdation,
     syncUserDeletion
+    /* your functions will be passed here later! */
   ],
 });
